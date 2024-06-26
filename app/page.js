19c0fec6547a73bbe1042c405897f9347c5c6120
octaviusthe3rd <admirechaga@gmail.com>
2024-06-26 18:34:06 +0200

    homepage complete

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
+'use client'
+import Footer from "@/Components/Footer";
+import BlogList from "@/Components/BlogList";
 import Header from "@/Components/header";
 
 export default function Home() {
   return (
     <>
       <Header/>
+      <BlogList/>
+      <Footer/>
     </>
   );
 }
